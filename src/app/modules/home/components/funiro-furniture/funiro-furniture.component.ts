c87925397da76a5608bcf0a7944ed5bd1a839486
Ibrahim Hassan <103008404+IbrahimHassanHanafy@users.noreply.github.com>
2024-03-24 16:25:15 +0200

    save

--- src/app/modules/home/components/funiro-furniture/funiro-furniture.component.ts
+++ src/app/modules/home/components/funiro-furniture/funiro-furniture.component.ts
@@ -7,32 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funiro-furniture.component.css'],
 })
 export class FuniroFurnitureComponent implements OnInit {
-  data: Product[] = [];
+
   /**
    *
    */
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.LoadDAta();
+
   }
 
-  LoadDAta() {
-    this.http.get('https://localhost:7032/api/Products').subscribe({
-      next: (value: any) => {
-        this.data = value;
-        console.log(this.data);
-        
-      },
-      error: (err: any) => {
-        console.log(err);
-      },
-    });
-  }
+  
 }
-export interface Product{
-  name:string,
-  id:number,
-  description:string,
-  categoryId:string,
-  category:string
-}
+
